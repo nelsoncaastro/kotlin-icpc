6a506591e5e1429e525f167b226b82cbb48844d1
Unknown <00043516@uca.edu.sv>
2019-05-10 21:08:08 -0600

    Nicer looks

--- UVA-272/UVA-272.kt
+++ UVA-272/UVA-272.kt
@@ -3,16 +3,18 @@ fun main(Args: Array<String>){
     var first = true
     val strbuilder = StringBuilder()
     for (c in phrase){
-        if (c != '\"') {
-            strbuilder.append(c)
-        } else if (first){
-            strbuilder.append('`')
-            strbuilder.append('`')
-            first = !first
-        } else {
-            strbuilder.append('\'')
-            strbuilder.append('\'')
-            first = !first
+        when {
+            c != '\"' -> strbuilder.append(c)
+            first -> {
+                strbuilder.append('`')
+                strbuilder.append('`')
+                first = !first
+            }
+            else -> {
+                strbuilder.append('\'')
+                strbuilder.append('\'')
+                first = !first
+            }
         }
     }
     println(strbuilder.toString())
