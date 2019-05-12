4e9d3a8d5a1058b068f389dcd86e2081b2ca844d
Nelson Castro <00043516@uca.edu.sv>
2019-05-12 00:59:36 +0000

    Added correct implementation of readLine

--- UVA-272/UVA-272.kt
+++ UVA-272/UVA-272.kt
@@ -1,33 +1,35 @@
 import java.io.File
+import java.util.Scanner
 
-fun main(Args: Array<String>){
+fun main(Args: Array<String>) = with(Scanner(System.`in`)){
 
     /*
     Dirección de relativa de mi archivo de prueba.
     val testFile = ".\\src\\kotlin-icpc\\UVA-272\\poem.txt"
     Lectura directa de consola.
-    val phrase = readLine()!!
-     */
-
-    //Leer del archivo de la ruta especificada al correr el ejecutable.
+    Leer del archivo de la ruta especificada al correr el ejecutable.
     val phrase = File(Args[0]).readText()
+    */    
+    while(hasNext()){
+        val phrase = nextLine()
 
-    var first = true
-    val strbuilder = StringBuilder()
-    for (c in phrase){
-        when {
-            c != '\"' -> strbuilder.append(c)
-            first -> {
-                strbuilder.append('`')
-                strbuilder.append('`')
-                first = !first
-            }
-            else -> {
-                strbuilder.append('\'')
-                strbuilder.append('\'')
-                first = !first
+        var first = true
+        val strbuilder = StringBuilder()
+        for (c in phrase){
+            when {
+                c != '\"' -> strbuilder.append(c)
+                first -> {
+                    strbuilder.append('`')
+                    strbuilder.append('`')
+                    first = !first
+                }
+                else -> {
+                    strbuilder.append('\'')
+                    strbuilder.append('\'')
+                    first = !first
+                }
             }
         }
+        println(strbuilder.toString())
     }
-    println(strbuilder.toString())
 }
